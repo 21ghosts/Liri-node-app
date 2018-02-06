9e2595bfc03f4f7fc865f1602c6b649dd7214b49
21ghosts <nathanmyoung44@gmai.com>
2018-02-06 13:36:25 -0500

    late commit changes personal changes to come

---
+++ keys.js
@@ -0,0 +1,10 @@
+exports.twitterKeys = {
+    consumer_key: '8Ocf5xFvbTLWjqPvKXud69syL',
+    consumer_secret: 'lhmBZvMqX99oddS1Jd391ZFnDJcZRofRfdwUXP0NJMvw4kSEMJ',
+    access_token_key: '701802540187447296-nho7DRz46kimgd0siQFTihuMLkivk0K',
+    access_token_secret: 'dPfvJJDjSind6IdnvfnoBRYoDo9UJHeP7Mf7IE8kdRTqS',
+}
+
+exports.spotifyKeys = {
+    id: 'fcc8a8c866df42888c3acf6578e52872',
+    secret: '9209b18566854d24b35e063a836d10fe',
